c30145094baa2a5d89c509ce2acec48827207b18
stepin105229 <62146661+stepin105229@users.noreply.github.com>
2020-12-06 11:10:59 +0530

    Create main.c

---
+++ implementation/main.c
@@ -0,0 +1,16 @@
+
+#include"ticket.h"
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(){
+printf("welcome to railway ticket reservation\n");
+printf("TRAIN AVAILABLE:HYD-MBR-RAI\n");
+int i;
+for(i=0;i<15;i++){
+
+booking();
+printticket();
+}
+return 0;
+}
