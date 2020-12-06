f0392c590fb497c26dcd01fc5e33b84e3e43595e
stepin105229 <62146661+stepin105229@users.noreply.github.com>
2020-12-06 10:43:41 +0530

    Update ticket.h

--- implementation/inc/ticket.h
+++ implementation/inc/ticket.h
@@ -1,1 +1,20 @@
+#ifndef TICKET_H_INCLUDED
+#define TICKET_H_INCLUDED
+
+int first=5,second=5,third=5;
+struct node
+{
+int age;
+int ticketno;
+char classofseat[7];
+char name[100];
+char dest[4];
+}s;
+void booking();
+void printticket();
+int availablecheck();
+
+
+
+#endif // TICKET_H_INCLUDED
 
